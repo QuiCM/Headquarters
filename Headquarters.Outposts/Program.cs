b3e428b92f97133dabaf5d63394bc81b0cd7bb44
Chris <2648373+QuiCM@users.noreply.github.com>
2017-12-10 15:01:43 +1030

    Adds the Outposts project. Outposts will be a major refactoring of HQ to work as a distributed system

---
+++ Headquarters.Outposts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Headquarters.Outposts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
