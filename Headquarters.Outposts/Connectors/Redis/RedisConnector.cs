71e39b63e20e275b3dacf34947105cf9cf2b1d7b
Chris <2648373+QuiCM@users.noreply.github.com>
2018-02-02 12:42:22 +1030

    Refactor Redis connector & components for cleaner solution

---
+++ Headquarters.Outposts/Connectors/Redis/RedisConnector.cs
@@ -0,0 +1,86 @@
+﻿using Headquarters.Communications;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Headquarters.Outposts
+{
+    /// <summary>
+    /// Implements <see cref="PubSubProvider"/> to provide pub/sub through Redis
+    /// </summary>
+    public class RedisConnector : PubSubProvider
+    {
+        private ConnectionMultiplexer _redis;
+        private Dictionary<StackExchange.Redis.RedisChannel, Action<ChannelBase, IPublication>> _channelMap;
+
+        public override Type ChannelType => typeof(RedisChannel);
+
+        public RedisConnector(Brain brain) : base(brain) { }
+
+        /// <summary>
+        /// Asynchronously connects to the Redis server described in the given connection string
+        /// </summary>
+        /// <param name="connectionString">Connection string following the requirements of https://stackexchange.github.io</param>
+        /// <returns></returns>
+        public override async Task ConnectAsync(string connectionString)
+        {
+            _redis = await ConnectionMultiplexer.ConnectAsync(connectionString);
+            _channelMap = new Dictionary<StackExchange.Redis.RedisChannel, Action<ChannelBase, IPublication>>();
+        }
+
+        /// <summary>
+        /// Publishes a given publication to the given channel
+        /// </summary>
+        /// <param name="channel"></param>
+        /// <param name="publication"></param>
+        /// <returns></returns>
+        public override async Task PublishAsync(ChannelBase channel, IPublication publication)
+        {
+            StackExchange.Redis.RedisChannel redisChannel = (RedisChannel)channel;
+            RedisValue message = (RedisPublication)publication;
+
+            StackExchange.Redis.ISubscriber subscriber = _redis.GetSubscriber();
+            await subscriber.PublishAsync(redisChannel, message);
+        }
+
+        /// <summary>
+        /// Subscribes the service to a given channel, invoking the given callback when messages are received
+        /// </summary>
+        /// <param name="channel"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public override async Task SubscribeAsync(ChannelBase channel, Action<ChannelBase, IPublication> callback)
+        {
+            StackExchange.Redis.RedisChannel redisChannel = (RedisChannel)channel;
+
+            if (_channelMap.ContainsKey(redisChannel))
+            {
+                _channelMap[redisChannel] = callback;
+            }
+            else
+            {
+                _channelMap.Add(redisChannel, callback);
+            }
+
+            StackExchange.Redis.ISubscriber subscriber = _redis.GetSubscriber();
+            await subscriber.SubscribeAsync(redisChannel, PublishCallback);
+        }
+
+        private void PublishCallback(StackExchange.Redis.RedisChannel channel, RedisValue message)
+        {
+            if (_channelMap.ContainsKey(channel))
+            {
+                _channelMap[channel]((RedisChannel)channel, (RedisPublication)message);
+            }
+        }
+
+        /// <summary>
+        /// Disposes the Redis connection
+        /// </summary>
+        public override void Dispose()
+        {
+            _redis.Dispose();
+        }
+    }
+}
---
+++ Headquarters.Outposts/Connectors/Redis/RedisPublication.cs
@@ -0,0 +1,199 @@
+﻿using Headquarters.Communications;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+
+namespace Headquarters.Outposts
+{
+    /// <summary>
+    /// Implements an <see cref="IPublication"/> for Redis by wrapping <see cref="RedisValue"/>
+    /// </summary>
+    public struct RedisPublication : IPublication, IEquatable<RedisPublication>, IComparable<RedisPublication>, IComparable, IConvertible
+    {
+        private RedisValue _value;
+
+        public int Length => _value.IsNullOrEmpty ? 0 : _value.ToString().Length;
+        
+        public IEnumerable<string> Message => new[] { _value.ToString() };
+
+        public static implicit operator RedisPublication(int? value)
+        {
+            return new RedisPublication { _value = value };
+        }
+
+        public static implicit operator RedisPublication(long? value)
+        {
+            return new RedisPublication { _value = value };
+        }
+
+        public static implicit operator RedisPublication(double value)
+        {
+            return new RedisPublication { _value = value };
+        }
+
+        public static implicit operator RedisPublication(string value)
+        {
+            return new RedisPublication { _value = value };
+        }
+
+        public static implicit operator RedisPublication(byte[] value)
+        {
+            return new RedisPublication { _value = value };
+        }
+
+        public static implicit operator RedisPublication(bool? value)
+        {
+            return new RedisPublication { _value = value };
+        }
+
+        public static implicit operator RedisPublication(RedisValue value)
+        {
+            return new RedisPublication { _value = value };
+        }
+
+        public static implicit operator RedisValue(RedisPublication value)
+        {
+            return value._value;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return ((IComparable)_value).CompareTo(obj);
+        }
+
+        public int CompareTo(RedisPublication other)
+        {
+            return _value.CompareTo(other._value);
+        }
+
+        public bool Equals(RedisPublication other)
+        {
+            return other._value == _value;
+        }
+
+        public TypeCode GetTypeCode()
+        {
+            return ((IConvertible)_value).GetTypeCode();
+        }
+
+        public bool ToBoolean(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToBoolean(provider);
+        }
+
+        public byte ToByte(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToByte(provider);
+        }
+
+        public char ToChar(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToChar(provider);
+        }
+
+        public DateTime ToDateTime(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToDateTime(provider);
+        }
+
+        public decimal ToDecimal(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToDecimal(provider);
+        }
+
+        public double ToDouble(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToDouble(provider);
+        }
+
+        public short ToInt16(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToInt16(provider);
+        }
+
+        public int ToInt32(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToInt32(provider);
+        }
+
+        public long ToInt64(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToInt64(provider);
+        }
+
+        public sbyte ToSByte(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToSByte(provider);
+        }
+
+        public float ToSingle(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToSingle(provider);
+        }
+
+        public string ToString(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToString(provider);
+        }
+
+        public object ToType(Type conversionType, IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToType(conversionType, provider);
+        }
+
+        public ushort ToUInt16(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToUInt16(provider);
+        }
+
+        public uint ToUInt32(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToUInt32(provider);
+        }
+
+        public ulong ToUInt64(IFormatProvider provider)
+        {
+            return ((IConvertible)_value).ToUInt64(provider);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return _value.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+
+        public static bool operator ==(RedisPublication left, RedisPublication right)
+        {
+            return left._value == right._value;
+        }
+
+        public static bool operator !=(RedisPublication left, RedisPublication right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator <(RedisPublication left, RedisPublication right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(RedisPublication left, RedisPublication right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(RedisPublication left, RedisPublication right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(RedisPublication left, RedisPublication right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+    }
+}
